ff920f7bb8ab56c1fc0cc7b61eb606944bf64907
taschnell <schnellteo@hotmail.com>
2024-05-14 22:10:38 -0700

    some formatting, Note that sensors need callibration

--- sensors/main.c
+++ sensors/main.c
@@ -15,15 +15,19 @@ int main(int argc, char* argv[])
 	{
 		printf("Motion sersor NULL\n");
 	}
+	size_t i = 0;
+
 	while(1)
 	{
 		imuDataGet( &stAngles, &stGyroRawData, &stAccelRawData, &stMagnRawData);
+		printf("%ld",i);
 		printf("\r\n /-------------------------------------------------------------/ \r\n");
 		printf("\r\n Roll: %.2f     Pitch: %.2f     Yaw: %.2f \r\n",stAngles.fRoll, stAngles.fPitch, stAngles.fYaw);
 		printf("\r\n Acceleration: X: %d     Y: %d     Z: %d \r\n",stAccelRawData.s16X, stAccelRawData.s16Y, stAccelRawData.s16Z);
 		printf("\r\n Gyroscope: X: %d     Y: %d     Z: %d \r\n",stGyroRawData.s16X, stGyroRawData.s16Y, stGyroRawData.s16Z);
 		printf("\r\n Magnetic: X: %d     Y: %d     Z: %d \r\n",stMagnRawData.s16X, stMagnRawData.s16Y, stMagnRawData.s16Z);
-		delay(100);
+		delay(250);
+		++i;
 	}
 	return 0;
 }
