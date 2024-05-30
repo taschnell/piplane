217a32da9dec109dc938078a3cfcd65a69d91c1e
taschnell <schnellteo@hotmail.com>
2024-05-30 14:26:41 -0700

    added imu to main

--- src/main.py
+++ src/main.py
@@ -3,22 +3,29 @@ import crsf_read
 import servo_test
 import pwm_motor
 import dshot_motor
+import bo085_pub
 
 def main():
     def run_servo_test():
         dshot_motor.main()
+
+    def run_imu():
+        bo085_pub.main()
 
     def run_crsf_read():
         crsf_read.main()
 
     servo_process = multiprocessing.Process(target=run_servo_test)
     crsf_process = multiprocessing.Process(target=run_crsf_read)
+    imu_process = multiprocessing.Process(target=run_imu)
 
     servo_process.start()
     crsf_process.start()
+    imu_process.start()
 
     servo_process.join()
     crsf_process.join()
+    imu_process.join()
 
 if __name__ == "__main__":
     main()
