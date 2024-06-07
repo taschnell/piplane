b33b502305e17fed1b7a60ae5a6f76c07de44c33
taschnell <schnellteo@hotmail.com>
2024-06-06 22:29:04 -0700

    for testing dshot on the pico

--- src/motor_pub_demo.py
+++ src/motor_pub_demo.py
@@ -9,27 +9,40 @@ class IntArrayPublisher(Node):
         self.publisher_ = self.create_publisher(Int32MultiArray, 'motor_array', 10)
 
         # Get user input once during initialization
-        self.array_data = []
+        self.target_values = []
         for i in range(4):
             while True:
                 try:
                     value = int(input(f"Enter an integer between 0 and 2047 for motor {i}: "))
                     if 0 <= value <= 2047:
-                        self.array_data.append(value)
+                        self.target_values.append(value)
                         break
                     else:
                         self.get_logger().info('Value must be between 0 and 2047. Try again.')
                 except ValueError:
                     self.get_logger().info('Invalid input. Enter an integer.')
 
-        self.timer = self.create_timer(1.0, self.timer_callback)
+        # Initialize current values to zero
+        self.current_values = [0] * 4
+
+        # Set the increment step and the publishing rate (timer interval)
+        self.increment_step = 1
+        self.timer = self.create_timer(0.1, self.timer_callback)
 
     def timer_callback(self):
         array = Int32MultiArray()
-        array.data = self.array_data
+
+        # Gradually increase each motor value to the target value
+        
+        array.data = self.current_values
 
         self.publisher_.publish(array)
         self.get_logger().info(f'Publishing: {array.data}')
+
+        for i in range(4):
+            if self.current_values[i] < self.target_values[i]:
+                self.current_values[i] = min(self.current_values[i] + self.increment_step, self.target_values[i])
+
 
 def main(args=None):
     rclpy.init(args=args)
