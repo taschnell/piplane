22994fb9f165ecb7a55f9b7ff297a92b60416ebd
taschnell <schnellteo@hotmail.com>
2024-06-17 13:21:29 -0700

    remove env

--- src/imu_pub.py
+++ src/imu_pub.py
@@ -16,16 +16,20 @@ from adafruit_bno08x.i2c import BNO08X_I2C
 class ImuPublisher(Node):
 
     def __init__(self):
-        super().__init__('imu_publisher')
-        self.get_logger().info('Initializing IMU Publisher Node')
-        
-        self.imu_pub = self.create_publisher(Imu, '/imu_data', 1)  # Topic: /imu_data, queue size: 10
-        self.timer = self.create_timer(0.01, self.publish_data)  # Publish every 0.01 seconds
+        super().__init__("imu_publisher")
+        self.get_logger().info("Initializing IMU Publisher Node")
+
+        self.imu_pub = self.create_publisher(
+            Imu, "/imu_data", 1
+        )  # Topic: /imu_data, queue size: 10
+        self.timer = self.create_timer(
+            0.01, self.publish_data
+        )  # Publish every 0.01 seconds
 
         i2c = busio.I2C(board.SCL, board.SDA)
         self.bno = BNO08X_I2C(i2c)
 
-        self.get_logger().info('Enabling BNO08X features')
+        self.get_logger().info("Enabling BNO08X features")
         self.bno.enable_feature(BNO_REPORT_ACCELEROMETER)
         self.bno.enable_feature(BNO_REPORT_GYROSCOPE)
         # self.bno.enable_feature(BNO_REPORT_MAGNETOMETER)
@@ -34,23 +38,26 @@ class ImuPublisher(Node):
     def publish_data(self):
         imu_msg = Imu()
 
-        accel_x, accel_y, accel_z = self.bno.acceleration 
-        gyro_x, gyro_y, gyro_z = self.bno.gyro 
+        accel_x, accel_y, accel_z = self.bno.acceleration
+        gyro_x, gyro_y, gyro_z = self.bno.gyro
         quat_i, quat_j, quat_k, quat_real = self.bno.quaternion
 
         imu_msg.linear_acceleration.x = accel_x
         imu_msg.linear_acceleration.y = accel_y
-        imu_msg.linear_acceleration.z = accel_z # Positive is down
+        imu_msg.linear_acceleration.z = accel_z  # Positive is down
         imu_msg.angular_velocity.x = gyro_x
         imu_msg.angular_velocity.y = gyro_y
         imu_msg.angular_velocity.z = gyro_z
-        imu_msg.orientation.w = quat_real
+        imu_msg.orientation.w = quat_real  # What is a quaternion really?
         imu_msg.orientation.x = quat_i
         imu_msg.orientation.y = quat_j
         imu_msg.orientation.z = quat_k
 
-        self.get_logger().info(f'Publishing IMU data: Accel=({accel_x}, {accel_y}, {accel_z})')
+        self.get_logger().info(
+            f"Publishing IMU data: Accel=({accel_x}, {accel_y}, {accel_z}), Gyro=({gyro_x}, {gyro_y}, {gyro_z}), Quaternion=({quat_i}, {quat_j}, {quat_k}, {quat_real})"
+        )
         self.imu_pub.publish(imu_msg)
+
 
 def main():
     rclpy.init()
@@ -58,5 +65,6 @@ def main():
     rclpy.spin(node)
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
