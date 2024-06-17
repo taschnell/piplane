7e5ac0d94f5aecfcfcb86e28f76f70aeb83444c8
taschnell <schnellteo@hotmail.com>
2024-06-16 22:33:57 -0700

    Started implementing ANGLE Mode

--- src/controller.py
+++ src/controller.py
@@ -17,8 +17,8 @@ ROLL = 0  # Continuous | 988 - 2011
 PITCH = 1  # Continuous | 988 - 2011
 THROTTLE = 2  # Continuous | 988 - 2011
 YAW = 3  # Continuous | 988 - 2011
-ARM = 4  # Two Modes | 988, 2011
-MODE = 5  # Three Modes | 988, 1500, 2011
+ARM = 4  # Two Modes | 988, 2011 | FALSE, TRUE
+MODE = 5  # Three Modes | 988, 1500, 2011 | "ANGLE", TBD, "ACRO" 
 RIGHT_SWITCH = 6  # Two Modes | 988, 2011
 RIGHT_SWITCH_2 = 7  # Three Modes | 988, 1500, 2011
 BUTTON_1 = 8  # OFF/ON | 988, 2011
@@ -29,8 +29,8 @@ BUTTON_4 = 11  # OFF/ON | 988, 2011
 
 EXPO = 2
 
-
 class MotorMap(IntEnum):
+    # Isn't it great how hardware goes from 1-4, but Programming indexs are from 0-3
     MOTOR_1 = 0
     MOTOR_2 = 1
     MOTOR_3 = 2
@@ -41,11 +41,10 @@ class Throttle_Publisher(Node):
 
     def __init__(self):
         super().__init__("motor_array")
-        self.publisher_ = self.create_publisher(Int16MultiArray, "motor_array", 1)
+        self.publisher_dshot = self.create_publisher(Int16MultiArray, "motor_array", 1)
 
         self.current_values = [0] * 4
 
-        self.timer = self.create_timer(0.01, self.timer_callback)
 
         self.subscription = self.create_subscription(
             Int16MultiArray, "crsf_channels_data", self.crsf_callback, 1
@@ -54,8 +53,11 @@ class Throttle_Publisher(Node):
             Imu, "imu_data", self.imu_callback, 1
         )
 
+        self.timer = self.create_timer(0.01, self.timer_callback)
+
         self.crsf_channels = [0] * 16
         self.ARMED = False
+        self.MODE = "ANGLE" # Can also Be ACRO
         self.euler_orientation = None
         self.linear_acc = None
         self.angular_vel = None
@@ -72,8 +74,7 @@ class Throttle_Publisher(Node):
         self.euler_orientation = self.quaternion_to_euler(
             [quat.w, quat.x, quat.y, quat.z]
         )
-        # self.get_logger().info(
-        #     f"Received IMU data: {self.linear_acc}, {self.angular_vel}, {self.euler_orientation}"
+        # self.get_logger().info(f"{self.euler_orientation}"
         # )
 
     def timer_callback(self):
@@ -85,40 +86,55 @@ class Throttle_Publisher(Node):
         elif self.ARMED and self.crsf_channels[ARM] == 988:
             self.ARMED = False
 
+        # Flight Mode, add automated mode later?
+        if self.crsf_channels[MODE] < 1500:
+            self.MODE = "ANGLE"
+        elif self.crsf_channels[MODE] > 1500:
+            self.MODE = "ACRO"
+
         if self.ARMED:
             throttle_value = self.crsf_channels[THROTTLE]
             mapped_throttle_value = self.exponential_mapping(throttle_value, EXPO)
 
             self.current_values = [mapped_throttle_value] * 4
-            self.throttle_transformation()
+            
+            # Angle or Acro Mode, maybe add others later.
+            if self.MODE == "ACRO":
+                self.acro_throttle_transformation()
+            elif self.MODE == "ANGLE":
+                self.angle_throttle_transformation()
 
             for i in range(4):
+                # Min Value of 100, Max of 2047 when armed
                 if self.current_values[i] > 2047:
                     self.current_values[i] = 2047
                 elif self.current_values[i] < 100:
                     self.current_values[i] = 100
 
             array.data = self.current_values
-            self.publisher_.publish(array)
-            self.get_logger().info(f"Publishing: {array.data}")
 
         else:
+            # Motors set to zero if not armed
             self.current_values = [0, 0, 0, 0]
             array.data = self.current_values
-            self.publisher_.publish(array)
-            self.get_logger().info(f"Publishing: {array.data}, NOT ARMED")
+
+        self.publisher_dshot.publish(array)
+        self.get_logger().info(f"Publishing: {array.data} | ARMED: {self.ARMED} | MODE: {self.MODE}")
+
 
     def exponential_mapping(self, value, expo=2):
+        # Isn't math cool!?
         normalized_value = (value - 988) / (2012 - 988)
         expo_value = normalized_value**expo
-        return int(expo_value * 2047 + 100)
+        return int(expo_value * 1947 + 100)
 
-    def percent_map(self, value, expo=2):
+    def percent_map(self, value, expo=2, rate=15):
         """Now with EXPO"""
         input_range = (988, 1500, 2012)
-        output_range = (-15, 0, 15)
+        output_range = (-rate, 0, rate)
 
         if not input_range[0] <= value <= input_range[-1]:
+            # TODO Add failsafe on Pico to stop motors if no more pub data is recieved
             raise ValueError(
                 f"Input value {value} is out of the range [{input_range[0]}, {input_range[-1]}]"
             )
@@ -139,10 +155,57 @@ class Throttle_Publisher(Node):
 
         return mapped_value / 100.0
 
-    def throttle_transformation(self):
+    
+
+
+    def angle_throttle_transformation(self):
         """
-        I'll document this eventually
+        Angle Mode, changes drone's angle by a maximum of 25 degrees relative to level, in pitch and roll direction, also handles yaw
+
+        These controls might end up inverted depending on IMU Position, make sure to test that
+
+        Should adjust motor speed less drastically if lower error value, and higher if a greater error value
         """
+        DEGREES = 25
+        RATE = 100
+        
+        roll_angle = self.percent_map(self.crsf_channels[ROLL], EXPO, RATE) * DEGREES
+        pitch_angle = self.percent_map(self.crsf_channels[PITCH], EXPO, RATE) * DEGREES
+        yaw_per = self.percent_map(self.crsf_channels[YAW], EXPO, RATE)
+
+        imu_pitch = self.euler_orientation[0]
+        imu_yaw = self.euler_orientation[1]
+        imu_roll = self.euler_orientation[2]
+
+        roll_err = roll_angle - imu_roll
+        pitch_err = pitch_angle - imu_pitch
+
+        self.get_logger().info(f"{roll_err}, {pitch_err}")
+
+
+        # # Note this works because all throttle values are initially the same
+        # roll_vals = round(self.current_values[MotorMap.MOTOR_1] * roll_per)
+        # pitch_vals = round(self.current_values[MotorMap.MOTOR_1] * pitch_per)
+        # yaw_vals = round(self.current_values[MotorMap.MOTOR_1] * yaw_per)
+
+        # self.current_values[MotorMap.MOTOR_1] = (
+        #     self.current_values[MotorMap.MOTOR_1] - roll_vals + pitch_vals + yaw_vals
+        # )
+        # self.current_values[MotorMap.MOTOR_2] = (
+        #     self.current_values[MotorMap.MOTOR_2] - roll_vals - pitch_vals - yaw_vals
+        # )
+        # self.current_values[MotorMap.MOTOR_3] = (
+        #     self.current_values[MotorMap.MOTOR_3] + roll_vals + pitch_vals - yaw_vals
+        # )
+        # self.current_values[MotorMap.MOTOR_4] = (
+        #     self.current_values[MotorMap.MOTOR_4] + roll_vals - pitch_vals + yaw_vals
+        # )
+
+    def acro_throttle_transformation(self):
+        """
+        Acrobatic Mode, changes motor throttle by up to 15% per axis. Still needs to stop the drone from spinning by using IMU Angular Velocity
+        """
+        
         roll_per = self.percent_map(self.crsf_channels[ROLL], EXPO)
         pitch_per = self.percent_map(self.crsf_channels[PITCH], EXPO)
         yaw_per = self.percent_map(self.crsf_channels[YAW], EXPO)
