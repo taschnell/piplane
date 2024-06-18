7497b8acf5492f771ef2f88be646b405e740586d
taschnell <schnellteo@hotmail.com>
2024-06-18 11:38:59 -0700

    added ros2 workspace

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'piplane'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='taschnell',
+    maintainer_email='schnellteo@hotmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'crsf_node = src.crsf_pub:main'
+        ],
+    },
+)
